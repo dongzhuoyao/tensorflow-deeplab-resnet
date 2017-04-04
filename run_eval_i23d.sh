a62a810bb39d031c694ec9a776f62fb74c4ec462
dongzhuoyao <929109550@qq.com>
2017-04-04 09:14:40 +0800

    t

--- run_eval_i23d.sh
+++ run_eval_i23d.sh
@@ -1,4 +1,4 @@
-python evaluate.py --data-dir  \
+python evaluate.py --data-dir  /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012  \
 --data-list  dataset/val.txt \
 --restore-from snapshots/model.ckpt-20000.meta \
 --num-steps 1449

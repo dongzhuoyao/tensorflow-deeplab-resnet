25d611b84ac033ce66e83e986e06bfcc4b827aa9
dongzhuoyao <929109550@qq.com>
2017-04-07 23:03:39 +0800

    t

--- run_train_3ka.sh
+++ run_train_3ka.sh
@@ -1,2 +1,2 @@
-python train.py --data-dir /home/hutao1/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
+CUDA_VISIBLE_DEVICES=2  python train.py --data-dir /home/hutao1/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
  --random-scale

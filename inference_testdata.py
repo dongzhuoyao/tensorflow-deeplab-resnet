c05e817c8255bbc30dadea1abb7255ce00b655dd
dongzhuoyao <929109550@qq.com>
2017-04-07 21:10:03 +0800

    t

--- inference_testdata.py
+++ inference_testdata.py
@@ -59,7 +59,7 @@ def main():
         img = tf.image.decode_jpeg(tf.read_file(img_path), channels=3)
         # Convert RGB to BGR.
         img_r, img_g, img_b = tf.split(value=img, num_or_size_splits = 3, axis = 2)
-        img = tf.cast(tf.concat(2, [img_b, img_g, img_r]), dtype=tf.float32)
+        img = tf.cast(tf.concat([img_b, img_g, img_r],2), dtype=tf.float32)
         # Extract mean.
         img -= IMG_MEAN
 

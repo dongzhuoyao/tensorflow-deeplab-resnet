a3b6acdd3ad357d5ff2914e027bf80968a7514fc
dongzhuoyao <929109550@qq.com>
2017-04-07 21:21:57 +0800

    t

--- run_inference_testdata_i23d.sh
+++ run_inference_testdata_i23d.sh
@@ -1,3 +1,3 @@
-python inference_testdata.py --test-img-dir  /cdata/hut/dataset/pascalvoc2012/normal_voc2012/test  \
+python inference_testdata.py --test-img-dir  /cdata/hut/dataset/pascalvoc2012/normal_voc2012/test/*.jpg  \
 --model-weights snapshots/model.ckpt-20000 \
 --save-dir ./testdata_output

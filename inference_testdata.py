aa77c38aef94d87119d09061d528165730e6d49c
dongzhuoyao <929109550@qq.com>
2017-04-07 21:52:23 +0800

    t

--- inference_testdata.py
+++ inference_testdata.py
@@ -55,14 +55,12 @@ def main():
         print("save_dir not exist,mkdir..")
         os.makedirs(args.save_dir)
 
-    test_img_list = glob.glob(os.path.join(args.test_img_dir,"*.jpg"))
-    for img_path in test_img_list:
-
         # Prepare image.
+        img_path = tf.placeholder(tf.string)
         img = tf.image.decode_jpeg(tf.read_file(img_path), channels=3)
         # Convert RGB to BGR.
-        img_r, img_g, img_b = tf.split(value=img, num_or_size_splits = 3, axis = 2)
-        img = tf.cast(tf.concat([img_b, img_g, img_r],2), dtype=tf.float32)
+        img_r, img_g, img_b = tf.split(value=img, num_or_size_splits=3, axis=2)
+        img = tf.cast(tf.concat([img_b, img_g, img_r], 2), dtype=tf.float32)
         # Extract mean.
         img -= IMG_MEAN
 
@@ -74,10 +72,9 @@ def main():
 
         # Predictions.
         raw_output = net.layers['fc1_voc12']
-        raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2,])
+        raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2, ])
         raw_output_up = tf.argmax(raw_output_up, dimension=3)
         pred = tf.expand_dims(raw_output_up, dim=3)
-
 
         # Set up TF session and initialize variables.
         config = tf.ConfigProto()
@@ -91,17 +88,19 @@ def main():
         loader = tf.train.Saver(var_list=restore_var)
         load(loader, sess, args.model_weights)
 
+    test_img_list = glob.glob(os.path.join(args.test_img_dir,"*.jpg"))
+    for current_img_path in test_img_list:
         # Perform inference.
-        preds = sess.run(pred)
+        preds = sess.run(pred,feed_dict={img_path:current_img_path})
 
         msk = decode_labels(preds)
         im = Image.fromarray(msk[0])
         if not os.path.exists(args.save_dir):
             os.makedirs(args.save_dir)
 
-        img_name = os.path.basename(img_path)
+        img_name = os.path.basename(current_img_path)
         im.save(os.path.join(args.save_dir,img_name))
-        sess.close()
+
 
         print('The output file has been saved to {}'.format(os.path.join(args.save_dir,img_name)))
 

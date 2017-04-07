ac64229540eb3a4559663081f3505336ab0c839a
dongzhuoyao <929109550@qq.com>
2017-04-07 21:53:46 +0800

    t

--- inference_testdata.py
+++ inference_testdata.py
@@ -55,38 +55,38 @@ def main():
         print("save_dir not exist,mkdir..")
         os.makedirs(args.save_dir)
 
-        # Prepare image.
-        img_path = tf.placeholder(tf.string)
-        img = tf.image.decode_jpeg(tf.read_file(img_path), channels=3)
-        # Convert RGB to BGR.
-        img_r, img_g, img_b = tf.split(value=img, num_or_size_splits=3, axis=2)
-        img = tf.cast(tf.concat([img_b, img_g, img_r], 2), dtype=tf.float32)
-        # Extract mean.
-        img -= IMG_MEAN
+    # Prepare image.
+    img_path = tf.placeholder(tf.string)
+    img = tf.image.decode_jpeg(tf.read_file(img_path), channels=3)
+    # Convert RGB to BGR.
+    img_r, img_g, img_b = tf.split(value=img, num_or_size_splits=3, axis=2)
+    img = tf.cast(tf.concat([img_b, img_g, img_r], 2), dtype=tf.float32)
+    # Extract mean.
+    img -= IMG_MEAN
 
-        # Create network.
-        net = DeepLabResNetModel({'data': tf.expand_dims(img, dim=0)}, is_training=False)
+    # Create network.
+    net = DeepLabResNetModel({'data': tf.expand_dims(img, dim=0)}, is_training=False)
 
-        # Which variables to load.
-        restore_var = tf.global_variables()
+    # Which variables to load.
+    restore_var = tf.global_variables()
 
-        # Predictions.
-        raw_output = net.layers['fc1_voc12']
-        raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2, ])
-        raw_output_up = tf.argmax(raw_output_up, dimension=3)
-        pred = tf.expand_dims(raw_output_up, dim=3)
+    # Predictions.
+    raw_output = net.layers['fc1_voc12']
+    raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2, ])
+    raw_output_up = tf.argmax(raw_output_up, dimension=3)
+    pred = tf.expand_dims(raw_output_up, dim=3)
 
-        # Set up TF session and initialize variables.
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        sess = tf.Session(config=config)
-        init = tf.global_variables_initializer()
+    # Set up TF session and initialize variables.
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
 
-        sess.run(init)
+    sess.run(init)
 
-        # Load weights.
-        loader = tf.train.Saver(var_list=restore_var)
-        load(loader, sess, args.model_weights)
+    # Load weights.
+    loader = tf.train.Saver(var_list=restore_var)
+    load(loader, sess, args.model_weights)
 
     test_img_list = glob.glob(os.path.join(args.test_img_dir,"*.jpg"))
     for current_img_path in test_img_list:

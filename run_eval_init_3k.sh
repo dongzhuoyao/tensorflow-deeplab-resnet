0bc6ae7756b1f592afc91f77275832aa26f5a1c7
dongzhuoyao <929109550@qq.com>
2017-04-19 01:52:20 +0800

    t

--- run_eval_init_3k.sh
+++ run_eval_init_3k.sh
@@ -1,4 +1,4 @@
 python evaluate.py --data-dir  /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012  \
 --data-list  dataset/val.txt \
---restore-from deeplab_resnet.ckpt \
+--restore-from ./deeplab_resnet.ckpt \
 --num-steps 1449
